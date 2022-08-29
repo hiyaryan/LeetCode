44618d1df78cbc0caf838600b4763825b837ec37
Ryan Meneses <ryan@Ryans-iMac.local>
2022-08-29 08:59:15 -0700

    Add O(n) solution.

--- Algorithms/Python/1_two_sum.py
+++ Algorithms/Python/1_two_sum.py
@@ -32,28 +32,38 @@
 # Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
 class Solution(object):
-    # twoSum is n^2
-    def twoSum(self, nums, target):
+    # twoSum is O(n^2)
+    def twoSum2(self, nums, target):
         for i, n in enumerate(nums):
             for j, m in enumerate(nums[i + 1:]):      
                 if n + m == target:
                     return [i, j + i + 1]
 
-    # twoSum2 less than n^2
-    def twoSum2(self, nums, target):
-        print(nums)
-        print([_ for _ in nums if _ != nums[0]])
+    # twoSum2 less than O(n)
+    def twoSum(self, nums, target):
+        # 1. answer = [], return // stores two idices of integers that add to the target
+        # 2. while-loop while length(nums) > 0 over all nums from nums[-1:] 
+        # 3. If the sum of any of those elements equals the target then return answer
+        # 4. Otherwise: pop() the element
+        # 5. Check the next nums[-1:]--
+
+        i = len(nums) - 2
+        while (True):
+            if nums[i] + nums[-1] == target:
+                return [i, len(nums) - 1]
+
+            else:
+                nums.pop()
+                i = i - 1
         
-        pass  
+      
 
 s = Solution()
 print(s.twoSum([2,7,11,15], 9))
 print(s.twoSum([3,2,4], 6))
 print(s.twoSum([3,3], 6))
-print(s.twoSum([1,2,3,4,5,6,7,8,9], 12))
 
 
 # print(s.twoSum2([2,7,11,15], 9))
 # print(s.twoSum2([3,2,4], 6))
 # print(s.twoSum2([3,3], 6))
-# print(s.twoSum2([1,2,3,4,5,6,7,8,9], 12))
